2f7144e79cd3b2380e990861f1bb111c8d12a6cd
gavinmulya01 <113004302+gavinmulya01@users.noreply.github.com>
2022-09-21 07:35:40 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main(void) 
+{
+  float jumlah,harga,total;
+  cout <<"masukan jumlah barang =";
+  cin >> jumlah;
+  cout <<"masukan harga barang =Rp.";
+  cin >>harga;
+
+  cout <<"jumlah barang = "<<jumlah<<endl;
+  cout <<"harga barang per unit = "<<harga<<endl;
+
+    total=jumlah*harga;3
+    cout<<"\ntotal yang harus dibayar =Rp. "<< total;
+}
